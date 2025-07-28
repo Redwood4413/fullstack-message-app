15146771fe8a3b81536bfc9b56f7f9a2fe37d956
Redwood <redwoodredwoodowski@gmail.com>
2025-07-28 21:06:37 +0200

    feat: introduce login component

--- apps/web/src/components/LoginForm.vue
+++ apps/web/src/components/LoginForm.vue
@@ -1,91 +1,104 @@
 <script setup lang="ts">
-interface LoginForm {
-  email: string
-  password: string
-}
+import { loginSchema } from '@message-app/schemas/zod/user'
+import { useRegleSchema } from '@regle/schemas'
+import { useFocus } from '@vueuse/core'
+import type { QInput, QInputNativeElement } from 'quasar'
+import { onMounted, ref, useTemplateRef } from 'vue'
 
-import { useForm } from 'vee-validate'
-import { toTypedSchema } from '@vee-validate/zod'
-import { UserSchema } from '@message-app/shared-schemas'
+const form = ref({
+  email: '',
+  password: '',
+  isSelected: false,
+})
+const { r$ } = useRegleSchema(form, loginSchema)
 
-// const schema = toTypedSchema(UserSchema)
+const inputRef = useTemplateRef<QInput>('inputRef')
+const input = ref<QInputNativeElement>()
 
-// const { defineField,  } = useForm({
-// validationSchema: schema
-// })
+const { focused } = useFocus(input, { initialValue: true })
 
-// const [email] = defineField('email')
-// const [password] = defineField('password')
-// const {} = useForm({
-// validationSchema: schema
-// })
-
-const onSubmit = () => {
-  console.log('submit')
-}
-
-// const quasarConfig = (state) => ({
-//   props: {
-//     error: !!state.errors[0],
-//     'error-message': state.errors[0],
-//   },
-// });
+onMounted(() => {
+  input.value = inputRef.value?.getNativeElement()
+})
+const onSubmit = () => {}
 </script>
 <template>
   <QForm class="q-pt-xl q-gutter-lg column" @submit.prevent="onSubmit">
-    <div class="separator-container">
-      <QSeparator spaced="auto" />
+    <h1 class="text-weight-thin text-h2">
+      Sign in to begin
+      <span class="text-bg-clip gradient-primary text-weight-bolder">chatting.</span>
+    </h1>
+    <BaseSeparator>
       <RouterLink :to="{ name: 'register-form' }" class="text-overline">
         Not a member? <span class="text-bg-clip gradient-secondary">Sign up here</span>
       </RouterLink>
-      <QSeparator spaced="auto" />
-    </div>
-    <QInput v-model.trim="email" label="Your E-mail">
+    </BaseSeparator>
+    <QInput
+      v-model="form.email"
+      label="Your E-mail"
+      ref="inputRef"
+      no-error-icon
+      :error="r$.email.$error && !focused"
+      name="email"
+      bottom-slots
+    >
       <template v-slot:prepend>
         <QIcon name="mdi-account"></QIcon>
       </template>
-    </QInput>
-    <QInput v-model.trim="password" type="password" label="Your Password">
-      <template v-slot:prepend>
-        <QIcon name="mdi-lock"></QIcon>
+      <template v-slot:error>
+        <li v-for="error of r$.email.$errors" :key="error">
+          {{ error }}
+        </li>
       </template>
+      <template #before></template>
+      <template #counter></template>
     </QInput>
+    <div>
+      <QInput
+        v-model.trim="form.password"
+        type="password"
+        name="password"
+        ref="passwordRef"
+        bottom-slots
+        no-error-icon
+        :error="r$.password.$error"
+        label="Your Password"
+      >
+        <template v-slot:prepend>
+          <QIcon name="mdi-lock"></QIcon>
+        </template>
+        <template v-slot:error>
+          <li v-for="error of r$.password.$errors" :key="error">
+            {{ error }}
+          </li>
+        </template>
+      </QInput>
+      <RouterLink :to="{ name: 'reset-password' }" class="text-overline float-right"
+        >Forgot your password?
+        <span class="text-bg-clip gradient-secondary">Reset it</span></RouterLink
+      >
+    </div>
+
     <div class="flex justify-between">
-      <QCheckbox
-        v-model="isRememberSelected"
-        dense
-        label="Remember me"
-        color="secondary"
-      ></QCheckbox>
+      <QCheckbox v-model="form.isSelected" dense label="Remember me" color="secondary"></QCheckbox>
       <QBtn
         label="Sign in"
         color="secondary"
-        type="Login"
+        type="submit"
         no-caps
-        :to="{ name: 'main' }"
         icon-right="mdi-login"
+        :disabled="r$.$error || !r$.$anyDirty"
       ></QBtn>
     </div>
-    <div class="separator-container">
-      <QSeparator spaced="auto" />
+    <BaseSeparator>
       <span class="text-overline">or continue with</span>
-      <QSeparator spaced="auto" />
-    </div>
-
-    <QBtnGroup class="justify-center" flat>
-      <QBtn icon="mdi-google" title="Continue with Google" />
-      <QBtn icon="mdi-github" title="Continue with GitHub" />
-      <QBtn icon="mdi-facebook" title="Continue with Facebook" />
-      <QBtn icon="fa-brands fa-discord" title="Continue with Discord" />
-    </QBtnGroup>
+    </BaseSeparator>
+    <ContinueWith />
   </QForm>
 </template>
 
-<style scoped lang="scss">
-.separator-container {
-  display: grid;
-  grid-template-columns: 1fr max-content 1fr;
-  gap: 1rem;
-  padding-inline: 1rem;
+<style lang="scss" scoped>
+h1 {
+  text-wrap: balance;
 }
 </style>
