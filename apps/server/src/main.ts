68087d3c0b1ed2ff9211f082e9788c1696281cc6
Redwood <redwoodredwoodowski@gmail.com>
2025-08-13 13:37:48 +0200

    feat: add frontend url to CORS

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,6 +5,9 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Message App')
